217abb1cc5b5e455e1bb286a0a9aedc1205db95f
mitchmindtree <mitchell.nordine@gmail.com>
2015-03-22 19:39:48 +1100

    Made BitCrusher handle any number of channels and any Sample format

--- examples/test.rs
+++ examples/test.rs
@@ -1,38 +1,48 @@
+#![feature(collections)]
+
 extern crate bit_crusher;
 extern crate dsp;
 
-use dsp::{SoundStream, Event, Dsp};
+use dsp::{Dsp, Event, Settings, SoundStream};
 use bit_crusher::BitCrusher;
+
+const SAMPLE_HZ: u32 = 44_100;
+const FRAMES: u16 = 512;
+const CHANNELS: u16 = 2;
+const BIT_DEPTH: u8 = 16;
+const AMOUNT: f32 = 0.5;
 
 fn main() {
     
-    let mut sound_stream = SoundStream::<f32, f32>::new().run().unwrap();
+    let mut sound_stream = SoundStream::<f32, f32>::new()
+        .settings(Settings { sample_hz: SAMPLE_HZ, frames: FRAMES, channels: CHANNELS })
+        .run()
+        .unwrap();
 
-    let mut bit_crusher = BitCrusher::new(16, 0.5);
+    let mut bit_crusher = BitCrusher::new(BIT_DEPTH, AMOUNT, CHANNELS as usize);
 
+    // We'll use this to process the input buffer and pass the result to the output.
     let mut buffer = Vec::new();
 
+    // We'll use this to modulate the bit_crush amount.
     let mut time = 0.0;
 
     for event in sound_stream.by_ref() {
         match event {
-            Event::In(samples) => { 
-                buffer = samples;
-            },
-            
+            Event::In(input) => { ::std::mem::replace(&mut buffer, input); },
             Event::Out(output, settings) => {
                 bit_crusher.audio_requested(&mut buffer[..], settings);
                 output.clone_from_slice(&buffer[..]);
             },
-            
             Event::Update(dt) => {
                 use std::num::Float;
                 time += dt;
-                bit_crusher.amount = 0.5+(time as f32 * 0.5).sin()*0.5;
-                
-                println!("{}", bit_crusher.amount); 
-            }
+                let new_amount = 0.5 + (time as f32 * 0.5).sin() * 0.5;
+                bit_crusher.set_amount(new_amount);
+                println!("BitCrusher amount: {}", new_amount);
+            },
         }
     }
+
 }
 
